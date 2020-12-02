860b3e7ffcb5089714dd1781c6fadad1ca9a1716
kek-Sec <georgepetrakis99@gmail.com>
2020-12-02 02:36:12 +0200

    added required components and service

---
+++ src/app/get-news.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetNewsService {
+
+  constructor(private http:HttpClient) { }
+
+  getNews(): Observable<JSON> {
+    const url = `hhttps://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=74ce1a9a332d4f18ba349f36cb59f29f`;
+    return this.http
+      .get<JSON>(url)
+      .pipe(tap((_) => console.log(`got response!`)));
+  }
+}
